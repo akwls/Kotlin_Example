88c6a53e590ad0301734f3fbe0938d1e5fbcf182
Hajin Kim <rhrnak624@naver.com>
2022-01-11 20:14:02 +0900

    ch03 연산자

---
+++ src/ch03/AreaCalculate.kt
@@ -0,0 +1,7 @@
+package ch03
+
+fun main(args : Array<String>) {
+    var radius:Int = 10;
+    var area:Double = radius * radius * Math.PI;
+    println(area);
+}
---
+++ src/ch03/BMICalculate.kt
@@ -0,0 +1,7 @@
+package ch03
+
+fun main(args : Array<String>) {
+    var height:Double = 1.8;
+    var weight:Int = 80;
+    println(weight / (height * height));
+}
